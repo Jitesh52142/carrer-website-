174e9ffa71d843c1795f7b5d6c54d2ea2f477a5c
jitesh bawaskar <jiteshbawaskar05@gmail.com>
2025-03-08 00:22:38 +0530

    5th commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask,render_template
 
 app = Flask(__name__)## that show how the perticular scricpt is invoked 
 
@@ -6,9 +6,8 @@ app = Flask(__name__)## that show how the perticular scricpt is invoked
 @app.route("/")#they make and sattle the path of the link of the app after domain name.register the rpute to the application
 
 def helloworld():
-    return "hellow world"
+    return render_template('home.html')
 
 if __name__ =="__main__":
     app.run(host="0.0.0.0",debug = True)
 
-    
