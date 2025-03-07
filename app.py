17c8497f3f2631c364c6f163cbdd3ed96f807d57
jitesh bawaskar <jiteshbawaskar05@gmail.com>
2025-03-07 17:03:28 +0530

    the third commit

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("hellow world ")
+from flask import Flask 
+
+app = Flask(__name__)## that show how the perticular scricpt is invoked 
+
+
+@app.route("/")#they make and sattle the path of the link of the app after domain name.register the rpute to the application
+
+def helloworld():
+    return "hellow world"
+
+if __name__ =="__main__":
+    app.run(host="0.0.0.0",debug = True)
+
+    
